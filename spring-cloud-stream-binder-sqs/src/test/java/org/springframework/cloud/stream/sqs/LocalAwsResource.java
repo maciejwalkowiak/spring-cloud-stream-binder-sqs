608b19a308b8b9b9b85c7997139a732377fb3eff
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2019-03-01 20:12:42 +0100

    Delete SNS topic when test ends.

--- spring-cloud-stream-binder-sqs/src/test/java/org/springframework/cloud/stream/sqs/LocalAwsResource.java
+++ spring-cloud-stream-binder-sqs/src/test/java/org/springframework/cloud/stream/sqs/LocalAwsResource.java
@@ -2,7 +2,9 @@ package org.springframework.cloud.stream.sqs;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 
 import org.junit.rules.TestWatcher;
@@ -20,15 +22,15 @@ public class LocalAwsResource extends TestWatcher {
     private LocalStackContainer localstack = new LocalStackContainer().withServices(Service.SQS,
                                                                                     Service.SNS);
 
-    private AmazonSQSAsync sqsClient;
+    private AmazonSQS sqsClient;
+    private AmazonSNS snsClient;
+
 
     @Override
     protected void starting(Description description) {
         this.localstack.start();
-        AmazonSQSAsyncClientBuilder builder = AmazonSQSAsyncClientBuilder.standard();
-        builder.setEndpointConfiguration(localstack.getEndpointConfiguration(Service.SQS));
-        builder.setCredentials(localstack.getDefaultCredentialsProvider());
-        this.sqsClient = builder.build();
+        this.sqsClient = createSqsClient();
+        this.snsClient = createSnsClient();
     }
 
     @Override
@@ -36,7 +38,9 @@ public class LocalAwsResource extends TestWatcher {
         sqsClient.listQueues()
                  .getQueueUrls()
                  .forEach(queueUrl -> sqsClient.deleteQueue(queueUrl));
-        // TODO: delete SNS topics
+        snsClient.listTopics()
+                 .getTopics()
+                 .forEach(topic -> snsClient.deleteTopic(topic.getTopicArn()));
         localstack.stop();
     }
 
@@ -46,5 +50,19 @@ public class LocalAwsResource extends TestWatcher {
 
     public AWSCredentialsProvider getCredentialsProvider() {
         return localstack.getDefaultCredentialsProvider();
+    }
+
+    private AmazonSQS createSqsClient() {
+        AmazonSQSAsyncClientBuilder builder = AmazonSQSAsyncClientBuilder.standard();
+        builder.setEndpointConfiguration(localstack.getEndpointConfiguration(Service.SQS));
+        builder.setCredentials(localstack.getDefaultCredentialsProvider());
+        return builder.build();
+    }
+
+    private AmazonSNS createSnsClient() {
+        AmazonSNSClientBuilder builder = AmazonSNSClientBuilder.standard();
+        builder.setEndpointConfiguration(localstack.getEndpointConfiguration(Service.SQS));
+        builder.setCredentials(localstack.getDefaultCredentialsProvider());
+        return builder.build();
     }
 }
